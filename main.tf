dcdf0b1dd7449e554ba6b74d26120aec719f80dd
Attila Sovak <attila.sovak@gmail.com>
2025-05-27 11:33:24 +0200

    Migrate to universal addon (Tempo) (#2)
    * feat(tempo): init files
    
    * feat(tempo): add tempo syntax and universal addon format
    
    * fix(tempo): fix iam coalesce and link ref

---
+++ main.tf
@@ -0,0 +1,41 @@
+/**
+ * # AWS EKS Tempo Terraform module
+ *
+ * A Terraform module to deploy the [Tempo](https://grafana.com/docs/tempo/latest/) on Amazon EKS cluster.
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-tempo/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-tempo/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-tempo/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-tempo/actions/workflows/pre-commit.yml)
+ */
+
+locals {
+  addon = {
+    name = "tempo"
+
+    helm_chart_name    = "tempo-distributed"
+    helm_chart_version = "1.6.14"
+    helm_repo_url      = "https://grafana.github.io/helm-charts"
+  }
+
+  addon_irsa = {
+    (local.addon.name) = {
+      irsa_policy_enabled = local.irsa_policy_enabled
+      irsa_policy         = var.irsa_policy != null ? var.irsa_policy : try(data.aws_iam_policy_document.this[0].json, "")
+    }
+  }
+
+  addon_values = yamlencode({
+    rbac = {
+      create = module.addon-irsa[local.addon.name].rbac_create
+    }
+
+    serviceAccount = {
+      create = module.addon-irsa[local.addon.name].service_account_create
+      name   = module.addon-irsa[local.addon.name].service_account_name
+      annotations = module.addon-irsa[local.addon.name].irsa_role_enabled ? {
+        "eks.amazonaws.com/role-arn" = module.addon-irsa[local.addon.name].iam_role_attributes.arn
+      } : tomap({})
+    }
+  })
+
+  addon_depends_on = []
+}
